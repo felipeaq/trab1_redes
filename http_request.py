0d53d9deda984359c9cadbecd1934639be6cc22d
felipeaq <felipeantunesquirino@gmail.com>
2019-09-02 10:49:49 -0300

    consulta de acoes

--- http_request.py
+++ http_request.py
@@ -1,31 +1,39 @@
 import socket
-
+import ssl
 class HttpRequest(socket.socket):
     def __init__(self):
         super().__init__(socket.AF_INET, socket.SOCK_STREAM)
 
     def make_request(self,link,server,port=80,message_lenght=256,max_len=2**20):
         data="erro na conexão com servidor: {}".format(server)
-        if not "http://" in link:
-            link="http://"+link
-        message="GET "+link+"\r\n"
-        print (message)
+        
+        
+        
         server_address = (server, port)
         try:
             self.connect(server_address)
         except:
             return "servidor não econtrado"
+        if port==80:
+            s=self
+            if not "http://" in link:
+                link="http://"+link
+            message="GET "+link+"\r\n\r\n"
+        if port ==443:
+            s=ssl.wrap_socket(self, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+            message="GET "+link+"\r\nHost:"+server+"\r\nConnection: close\r\n\r\n"
+        #print (message)
         try:
-            self.sendall(message.encode())
+            s.sendall(message.encode())
             temp="bla"
             data=b""
             l=0
             while temp and l<max_len:
-                temp=self.recv(message_lenght)
+                temp=s.recv(message_lenght)
                 data+=temp
                 l+=message_lenght
         finally:
-            self.close()
+            s.close()
         return data
 
 if __name__=="__main__":
@@ -34,5 +42,5 @@ if __name__=="__main__":
     economia.awesomeapi.com.br 80
     '''
     h=HttpRequest()
-    print (h.make_request("http://query1.finance.yahoo.com/v7/finance/options/AAPL","query1.finance.yahoo.com"))
+    print (h.make_request("/api/v3/stock/real-time-price/AAPL","financialmodelingprep.com",port=443).decode())
 
--- server.py
+++ server.py
@@ -6,21 +6,25 @@ import json
 from http_request import HttpRequest
 
 class ExternalFiles:
-    def __init__(self,book_file,dollar_server,dollar_link):
+    def __init__(self,book_file,dollar_server,dollar_link,stock_link,stock_server):
         self.book_file=book_file
         self.dollar_server=dollar_server
         self.dollar_link=dollar_link
+        self.stock_link=stock_link
+        self.stock_server=stock_server
 
 class Worker:
     def __init__(self,sock,conn,client,timeout):
         self.sock=sock
         self.conn=conn
         self.client=client
-        self.methods={"\\verlivros":self.books,"\\verdolar":self.dollar2real} 
+        self.methods={"\\verlivros":self.books,"\\verdolar":self.dollar2real,"\\veracao":self.stock_price} 
         self.timeout=timeout
         self.external_files=ExternalFiles("bibliografias.txt",
             "economia.awesomeapi.com.br",
-            "economia.awesomeapi.com.br/all/USD-BRL")
+            "economia.awesomeapi.com.br/all/USD-BRL",
+            "/api/v3/stock/real-time-price/",
+            "financialmodelingprep.com")
 
     def get_message(self,data):
         message_list=[]
@@ -69,7 +73,25 @@ class Worker:
         return message
     
     def stock_price(self,*args):
-        pass
+        if len(args)!=1:
+            return "para ver o preço de ações deve ser passado o nome de uma \
+                exemplo: \\veracao AAPL"
+        h=HttpRequest()
+        try:
+            temp= h.make_request(self.external_files.stock_link+args[0],self.external_files.stock_server,port=443)
+        except:
+            print (self.external_files.stock_server.encode())
+            return "impossível conectar ao servidor de terceiro\n"
+        try:
+            d=json.loads(temp)["price"]
+        except KeyError:
+            return "Ação {} não encontrada\n".format(args[0])
+        except json.JSONDecodeError:
+            return "Recebemos uma mensagem estranha do servidor externo, nossos engenheiros estão trabalhando nisso".format(args[0])
+        except Exception as e:
+            print (temp)
+            return "Problema com o servidor externo\n"
+        return "preço da ação "+args[0]+": "+ str(d)+"\n"
 
 class Server:
     def __init__(self,ip,port,n_threads,timeout):
